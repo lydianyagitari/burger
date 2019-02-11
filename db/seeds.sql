8a6727d3f797fd2bb6f39eed413d2aa1d498bccb
unknown <mzinnocents96@gmail.com>
2019-02-11 11:22:19 -0600

    update

---
+++ db/seeds.sql
@@ -0,0 +1,13 @@
+-- Select burgers_db
+
+USE `burgers_db`;
+
+
+
+-- insert 3 burgers
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('whopper', false, CURRENT_TIMESTAMP);
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('quater pounder', false, CURRENT_TIMESTAMP);
+
+INSERT INTO `burgers` (burger_name, devoured, date) VALUES ('chicken', false, CURRENT_TIMESTAMP);
